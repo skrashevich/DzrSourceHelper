fe341669a4ee0dedb29effab312506452d941389
Vladick <vladick.kuharev@yandex.ru>
2025-11-19 12:38:28 +0300

    Add downloader from gdrive

--- download_files.py
+++ download_files.py
@@ -1,12 +1,11 @@
 import os
 import io
+import json
 
+import requests
 from googleapiclient.discovery import build
 from google.oauth2 import service_account
 from googleapiclient.http import MediaIoBaseDownload
-
-from dotenv import load_dotenv
-load_dotenv()
 
 
 
@@ -17,53 +16,40 @@ def authenticate():
     credentials = service_account.Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=SCOPES)
     return build('drive', 'v3', credentials=credentials)
 
-
-def download_file(service, file_id, file_name, download_path):
-    file_path = os.path.join(download_path, file_name)
-    os.makedirs(os.path.dirname(file_path), exist_ok=True)
-    
-    try:
-        request = service.files().get_media(fileId=file_id)
-        fh = io.FileIO(file_path, 'wb')
-        downloader = MediaIoBaseDownload(fh, request)
-        done = False
-        while done is False:
-            status, done = downloader.next_chunk()
-            print(f'Скачивание {file_name}: {int(status.progress() * 100)}%')
-    except Exception as e:
-        print(f'Ошибка при скачивании {file_name}: {e}')
+    # УДАЛИТЬ ПРОВЕРКА НА ЭНКЕ
+    # EN_URL = "https://koshmar.en.cx/Administration/Games/FileUploader.aspx?gid=76014"
+    # auth_user()
+    # print(file_path)
+    # binary_data = file_stream.getvalue()
+    # data = {
+    #     file_name: binary_data
+    # }
+    # resp = requests.post(EN_URL, cookies=get_cookies(), files=data)
+    # print(resp.text)
+    # УДАЛИТЬ ПРОВЕРКА НА ЭНКЕ
 
 
-def list_files_recursively(service, folder_id, local_base_path, current_path=""):
+def get_files_from_drive(service, folder_id):
     results = service.files().list(
         q=f"'{folder_id}' in parents",
-        fields="files(id, name, mimeType)"
+        fields="nextPageToken, files(id, name, mimeType)"
     ).execute()
     items = results.get('files', [])
-    
+
+    files_binaries = []
     for item in items:
         item_name = item['name']
         item_id = item['id']
-        relative_path = os.path.join(current_path, item_name)
-        full_local_path = os.path.join(local_base_path, relative_path)
-        
-        if item['mimeType'] == 'application/vnd.google-apps.folder':
-            print(f'Обработка папки: {relative_path}')
-            list_files_recursively(service, item_id, local_base_path, relative_path)
-        else:
-            print(f'Найден файл: {relative_path}')
-            download_file(service, item_id, item_name, os.path.dirname(full_local_path))
 
-
-def main():
-    FOLDER_ID = os.getenv('FOLDER_ID') 
-    LOCAL_DOWNLOAD_PATH = './downloads'
+        if 'image' in item['mimeType']:
+            print(f'Найден файл: {item_name}')
+            with io.BytesIO() as file_stream:
+                request = service.files().get_media(fileId=item_id)
+                downloader = MediaIoBaseDownload(file_stream, request)
+                done = False
+                while done is False:
+                    status, done = downloader.next_chunk()
+                    print(f'Скачивание {item_name}: {int(status.progress() * 100)}%')
+                files_binaries.append({item_name: file_stream.getvalue()})
     
-    service = authenticate()
-    print('Аутентификация успешна. Начинается скачивание...')
-    list_files_recursively(service, FOLDER_ID, LOCAL_DOWNLOAD_PATH)
-    print('Процесс скачивания завершен.')
-
-
-if __name__ == '__main__':
-    main()
+    return files_binaries
